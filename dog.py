3db9a38e57ed2010284fe34f64620e4a73c8c05c
Muhammad Tifak <moht20009@gmail.com>
2019-09-22 20:34:17 -0700

    Started superhero team dueler project

---
+++ dog.py
@@ -0,0 +1,10 @@
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+    def bark(self):
+        print("Woof!")
+    def sit(self):
+        print("<> sits")
+    def roll_over(self):
+        print("<> rolls over")
