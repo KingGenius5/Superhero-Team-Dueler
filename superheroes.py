240775e3ecfe59bcece51a6baa7dac59314c5040
Muhammad Tifak <moht20009@gmail.com>
2019-09-23 11:23:22 -0700

    Built out hero class and added armors

--- superheroes.py
+++ superheroes.py
@@ -6,23 +6,28 @@ class Ability:
         self.name = str(name)
         self.attack_strength = int(attack_strength)
     def attack(self):
-        attack_strength = randint(2,7)
-        return attack_strength
+        self = randint(2,7)
+        return self
 
 
 class Armor:
     def __init__(self, name, max_block):
         self.name = str(name)
         self.max_block =int(max_block)
-    def block():
-        print("Blockin")
+    def block(self):
+        self = randint(0, max_block)
+        return self
+
 
 class Hero:
-    def __init__(self, name, starting_health):
+    def __init__(self, name, starting_health=100):
         self.name = str(name)
         self.starting_health = int(starting_health)
+        self.current_health = starting_health
+        self.abilities = []
+        self.armors = []
     def add_ability():
-        print("This add an ability")
+        print("This adds an ability")
     def attack():
         print("This will attack")
     def defend():
@@ -34,7 +39,11 @@ class Hero:
     def fight():
         print("This requires an opponent in the hero class")
 
+
 if __name__ == "__main__":
     ability = Ability("Debugging Ability", 20)
     print(ability.name)
     print(ability.attack())
+    my_hero = Hero("Grace Hopper", 200)
+    print(my_hero.name)
+    print(my_hero.current_health)
